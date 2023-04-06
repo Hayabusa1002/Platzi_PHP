24cc523153d098635a78c7ed2b01404430da80fa
Luis Fuentes <LDFG1002@gmail.com>
2023-04-06 18:29:21 -0500

    declaración de variables en PHP

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    /* STRINGS */
     # NOTE: Each code line in PHP is separated with ';'
     # 1. To display something on the web page, PHP use 'echo'
     # 2. For strings PHP uses ' or "
@@ -8,4 +9,18 @@
     echo "Hola, mundo.<br> estoy usando PHP<br>";
     echo "Hola, mundo.\n estoy usando PHP\n";
     echo 'Hola, mundo.' . "\n" . 'estoy usando PHP';
+
+    /* VARIABLES */
+    # 6. To declare a variable first we type '$' then type the variable name
+    # 7. If we type a space, the following code doesn't belong to the variable name
+    # 8. We can combine variable values with 'echo'
+    # 9. In an 'echo', using " we can type the variables inside the string
+    # 10. If we type a variable inside the string using " and this variable is a complex element (such as an array) we have to use {}
+    $name       = 'Carlos';
+    $last_name  = 'Santana';
+    $age        = 25;
+    $array      = [ 'manzana', 'pera', 'uva' ]; 
+    echo 'No, no. Yo me llamo ' . $name . ' ' . $last_name . "<br>";
+    echo "Tengo $age años<br>";
+    echo "Mi fruta favorita es la {$array[0]}";
 ?>
