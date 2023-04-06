5842d263e50bd414850e38bad886de687425ec74
Luis Fuentes <LDFG1002@gmail.com>
2023-04-06 18:08:11 -0500

    principios básicos de strings en PHP

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-    echo "Hola, mundo."
+    # NOTE: Each code line in PHP is separated with ';'
+    # 1. To display something on the web page, PHP use 'echo'
+    # 2. For strings PHP uses ' or "
+    # 3. We can use HTML elements in PHP strings
+    # 4. Control characters do not work with ', but they work with " only when the code is excecuted from the terminal though; from the browser they do not work
+    # 5. To concatenate strings in PHP we use '.'
+    echo "Hola, mundo.<br> estoy usando PHP<br>";
+    echo "Hola, mundo.\n estoy usando PHP\n";
+    echo 'Hola, mundo.' . "\n" . 'estoy usando PHP';
 ?>
