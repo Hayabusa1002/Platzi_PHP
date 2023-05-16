eabd69243e0ebfeb6119c0890068adce6d51f8d2
Luis Daniel Fuentes Gonzalez <Luis.FuentesG@tigo.com.co>
2023-05-16 14:02:59 -0500

    Arreglos asociativos

--- explanation.php
+++ explanation.php
@@ -12,4 +12,37 @@
     echo "<br>";
     echo "Mientras que otra edad es " . $ages_1[1];
     echo "<br>";
+
+    echo "<br>-----------------------------------<br>";
+
+    /* ASOCIATIVE ARRAY */
+    # 3. Asociative arrays are like the arrays, but the keys of each value must be declared by the dev
+    # 4. To associate the key with the value we use '=>'
+    echo 'ASOCIATIVE ARRAY<br><br>';
+
+    $cafes = array(
+        "Capuccino" => 50,
+        "Latte" => 49,
+        "Americano" => 70
+    );
+    
+    echo "El precio del cafe Americano es de {$cafes['Americano']}";
+    echo "<br>";
+    
+    $personas = array(
+    
+        "Carlos" => array(
+            "edad" => 20,
+            "apellido" => "Santana"
+        ),
+    
+        "Mr.Michi" => array(
+            "edad" => 18,
+            "apellido" => "Michisancio"
+        ),
+    
+    );
+    
+    echo "La informacion de Mr. Michi es: Edad: " . $personas["Mr.Michi"]["edad"] . " Apellido: " . $personas["Mr.Michi"]["apellido"];
+    echo "<br>";
 ?>
