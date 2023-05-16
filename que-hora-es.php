9c84bd83504779991c9503075c4a7e3304113a74
Luis Daniel Fuentes Gonzalez <Luis.FuentesG@tigo.com.co>
2023-05-16 10:59:43 -0500

    Reto 2: h-m-s a segundos

--- que-hora-es.php
+++ que-hora-es.php
@@ -1,4 +1,5 @@
 <?php
+    // 1. Seconds to Hours, Minutes and Seconds
     $segundos   = readline("Ingresa el tiempo en segundos: "); 
 
     $horas      = (int) ($segundos / 3600);
@@ -7,5 +8,14 @@
     $segundos   = (int) ($segundos % 60); 
 
     echo "Son $horas horas, $minutos minutos y $segundos segundos."; 
-    echo "\n";
+    echo "\n\n";
+
+    // 2. Hours, Minutes and Seconds to Seconds
+    $horas      = readline("Por favor, ingresa el numero de horas: ");
+    $minutos    = readline("Por favor, ingresa los minutos: ");
+    $segundos   = readline("Por favor, ingresa los segundos: ");
+
+    $tiempo_en_segundos = ($horas * 60 * 60) + ($minutos * 60) + $segundos;
+
+    echo "Este tiempo equivale a $tiempo_en_segundos segundos. \n";
 ?>
